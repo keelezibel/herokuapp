c6f9a819cb69f3a821c4464a7b097a77636545fa
Le Tang <letang.boon@gmail.com>
2017-10-08 18:39:22 +0800

    added static file handling :

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^gggp/', include('webapp.urls')),
     url(r'^$', include('personal.urls')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+
